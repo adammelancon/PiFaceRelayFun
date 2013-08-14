0a9bec21e3fa2ddabe6fee13609aabae55561512
Adam Melancon <adammelancon@gmail.com>
2013-08-14 14:34:50 -0500

    Fixed an issue where beautifulsoup wasnt getting new data because the page read wasn't in the loop. Added some debugging print statements that need to be commented out soon.

--- 911light.py
+++ 911light.py
@@ -4,13 +4,12 @@ from bs4 import BeautifulSoup
 import urllib2
 import time
 import piface.pfio as pfio
+from time import gmtime, strftime
 
 pfio.init()
 
 #this url is the middle frame with the incident list at lafayette911.org
-url = "http://67.32.159.27/webcad/webcad.asp"
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
+#soup = BeautifulSoup(page.read())  # Moved into the loop
 
 # A class to controll the relay
 class Bpress:
@@ -28,11 +27,6 @@ class Bpress:
             if pfio.digital_read(1) == 0:
                 return
 
-    def exitscript(self):
-        pfio.digital_write(1,0)
-        print "Exiting. Thanks for playing!"
-        exit()
-
 piface = Bpress()
 
 # Relay Test
@@ -43,25 +37,63 @@ piface = Bpress()
 
 
 def get_incidents():
-    # Create an emapty list for incidents
+    url = "http://67.32.159.27/webcad/webcad.asp"
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page.read())
     incidents = []
-    lookfor = "JOHNSTON"
+
 
     # Find the second table which has the incidents and find the individual row$
-    for i in soup.findAll('table')[1].findAll('tr'):
-        incidents.append(i.get_text()) # Add the text from the row to the list
+    try:
+        print "New Incident Loop:\n" + str(incidents)
+        lookfor = "AMBASSADOR"
+        for i in soup.findAll('table')[1].findAll('tr'):
+            incidents.append(i.get_text()) # Add the text from the row to the list
+
+    except IndexError:
+        print "No incidents on" + " " + lookfor
+        #piface.relayoff()
+        print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        print str(len(incidents) - 1) + " " + "Incidents"
+        print str(incidents)
+        print "=" * 20
+        incidents = []
+        print "******"
+        print "Incident after clear"
+        print incidents
+        print "******"
     
-    # Give it a road to look for incidents on to trigger the led.
     if filter(lambda x: lookfor in x,incidents):
         print "Incident reported on" + " " + lookfor
-        piface.relayon()
+       #piface.relayon()
+        print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        print str(len(incidents) - 1) + " " + "Incidents"
+        print str(incidents)
+        print "=" * 20
+        incidents = []
+        print "******"
+        print "Incident after clear"
+        print incidents
+        print "******"
+    
     else:
         print "No incidents on" + " " + lookfor
-        piface.relayoff()
-    time.sleep(30)
+       #piface.relayoff()
+        print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        print str(len(incidents) - 1) + " " + "Incidents"
+        print str(incidents)
+        print "=" * 20
+        incidents = []
+        print "******"
+        print "Incident after clear"
+        print incidents
+        print "******"
+
+
+    time.sleep(10)
 
 while True:
     get_incidents()
-    if pfio.digital_read(0) == 1:
-        piface.relayoff()
-        exit()
+    #if pfio.digital_read(0) == 1:
+    #    piface.relayoff()
+    #    exit()
